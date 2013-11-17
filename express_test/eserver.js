bb89fe3be720e88df797f7d5bad58981d3cafa83
Charles Julian Knight <charles@rabidaudio.com>
2013-11-17 00:16:20 -0500

    lovely abstraction

--- express_test/eserver.js
+++ express_test/eserver.js
@@ -1,50 +1,29 @@
 var express = require('express');
-var params = require('express-params');
-var app = express();
+//var params = require('express-params');
+
+//scripts
 var wrek = require('./wrek');
 
+//express settings
+var app = express();
+//app.use(express.json());
+//app.use(express.urlencoded()); //for POST
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.bodyParser());
 
 
-/*app.get('/wrek', function(req, res){
-    console.log('/wrek request');
-    res.send = wrek.run();
-});*/
+app.get( '/wrek/:id?', wrek.handle ); //DIR method
 
-/*app.get('/wrek', function(req, res){ // /^\/wrek\/[0-9]+$/
-    console.log('/wrek request');
-    res.send( wrek.run([]).stdout );
-});*/
+app.post('/post', function(req, res){
+    console.log("post req");
+    console.log(req.body);
+    
+    res.send("cool yo");
+});
 
-/*app.param(':id', function(d){
-    return parseInt(d);
-});*/
-
-app.get('/wrek/:id?', wrek.handle );
 
 console.log(app.routes);
 
 
 app.listen(3000);
-
-
-/*
-    FUNCTIONS
-    /function_name
-    /function_name  | (POST arguments)
-    /function_name/first_argument
-    /function_name?GET=arguments
-    
-    REST OBJECTS
-    /class_name/new                 -> return instanceid
-    /class_name/instanceid          -> return object (without methods)
-    /class_name/instanceid/property -> return instance.property
-    /class_name/instanceid/method   -> retrun result of
-        (ARGS: POST or GET)
-
-    LIBRARIES
-    /library/method
-    /library/method  | (POST arguments)
-    /library/method/first_argument
-    /library/method?GET=arguments
-
-*/
--- express_test/wrek.js
+++ express_test/wrek.js
@@ -1,30 +1,43 @@
 var sh = require('execSync');
 
-var required_args = [];
-var optional_args = ['id'];
 
-function run(args){
-    cmd = 'ruby app/wrek.rb';
+//depends
+var args = {
+    required: [],
+    optional: ['id']
+}
+var cmd = 'ruby app/wrek.rb';
+var methods = {dir: true, post: true, get: false];
+///////////////////////////////////////////////
+function _run(args){
     for(var i=0; i<args.length;i++)
         cmd = cmd + ' ' + args[i];
     return sh.exec(cmd);
 }
 
-function handle(req, res){
-    console.log('handle request');
-        console.log( req.route );
-    var args = [];
-    for(var i=0; i< required_args.length; i++){
-        if (req.params[required_args[i]] !== undefined)
-            args.push(req.params[required_args[i]]);
+function _handle(inputs, req, res){
+    var a = [];
+    for(var i=0; i< args.required.length; i++){
+        if (inputs[args.required[i]] !== undefined)
+            a.push(inputs[args.required[i]]);
     }
-    for(var i=0; i< optional_args.length; i++){
-        if (req.params[optional_args[i]] !== undefined)
-            args.push(req.params[optional_args[i]]);
+    for(var i=0; i< args.optional.length; i++){
+        if (inputs[args.optional[i]] !== undefined)
+            a.push(inputs[args.optional[i]]);
     }
     //console.log(req.params[optional_args[i]]);
-    console.log(args);
-    res.send( run(args).stdout );
+    //console.log(a);
+    res.send( _run(a).stdout );
 }
 
-exports.handle = handle
+function dir_handle(req, res){
+    console.log('DIR request');
+    _handle(req.params, req, res);
+}
+function post_handle(req, res){
+    console.log("POST request");
+    _handle(req.body, req, res);
+}
+
+if (methods.dir) exports.dir_handle = dir_handle;
+if (methods.post) exports.post_handle = post_handle;
