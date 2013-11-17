e5a1a23e6092814de954e6dfb871aa0581ee4c26
Charles Julian Knight <charles@rabidaudio.com>
2013-11-16 22:24:39 -0500

    progress

--- express_test/eserver.js
+++ express_test/eserver.js
@@ -5,58 +5,23 @@ var wrek = require('./wrek');
 
 
 
-
-/*app.run_callback = function(error, stdout, stderr) {
-    console.log("ER:" + error);
-    console.log("SO:" + stdout);
-    console.log("SE:" + stderr);
-    //if (error !== null)
-    this.parent.result = stdout;
-    //return stdout;
-}*/
-
-/*app.run = function(){
-  console.log("runapp called");
-  console.log(this);
-  require('child_process').exec('cat *.js bad_file | wc -l', this.run_callback);
-  //console.log(this.result);
-  //console.log("killing command");
-  //ls.kill('SIGHUP');
-  return this.result;
-}*/
-
-/*exec('cat *.js bad_file | wc -l',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
+/*app.get('/wrek', function(req, res){
+    console.log('/wrek request');
+    res.send = wrek.run();
 });*/
 
-
-
-
-
-app.get('/wrek', function(req, res){
-  console.log("request received");
-  res.send( wrek.run().stdout );
-});
-
-
-/*app.param('user', function(req, res, next, id){
-  User.find(id, function(err, user){
-    if (err) {
-      next(err);
-    } else if (user) {
-      req.user = user;
-      next();
-    } else {
-      next(new Error('failed to load user'));
-    }
-  });
+/*app.get('/wrek', function(req, res){ // /^\/wrek\/[0-9]+$/
+    console.log('/wrek request');
+    res.send( wrek.run([]).stdout );
 });*/
 
+/*app.param(':id', function(d){
+    return parseInt(d);
+});*/
+
+app.get('/wrek/:id?/:meow?', wrek.handle );
+
+console.log(app.routes);
 
 
 app.listen(3000);
--- express_test/wrek.js
+++ express_test/wrek.js
@@ -1,8 +1,17 @@
 var sh = require('execSync');
 
-function run(){
-    return sh.exec('ruby app/wrek.rb');
+function run(args){
+    cmd = 'ruby app/wrek.rb';
+    for(var i=0; i<args.length;i++)
+        cmd = cmd + ' ' + args[i];
+    return sh.exec(cmd);
 }
 
+function handle(req, res){
+    console.log('handle request');
+        console.log( req.route );
+        console.log( req.params );
+    res.send( run([]).stdout );
+}
 
 exports.run = run
