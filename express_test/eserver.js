14cb75b17e3a8ae8d80704f41dbe3d368f76fcdd
Charles Julian Knight <charles@rabidaudio.com>
2013-11-17 00:22:53 -0500

    working abstraction

--- express_test/eserver.js
+++ express_test/eserver.js
@@ -1,29 +1,21 @@
 var express = require('express');
-//var params = require('express-params');
 
 //scripts
 var wrek = require('./wrek');
 
 //express settings
 var app = express();
-//app.use(express.json());
-//app.use(express.urlencoded()); //for POST
 app.set('port', process.env.PORT || 3000);
-
-app.use(express.bodyParser());
+app.use(express.bodyParser());//for POST
 
 
 app.get( '/wrek/:id?', wrek.handle ); //DIR method
 
-app.post('/post', function(req, res){
+app.post('/post', function(req, res){ //POST method
     console.log("post req");
     console.log(req.body);
     
     res.send("cool yo");
 });
-
-
-console.log(app.routes);
-
 
 app.listen(3000);
