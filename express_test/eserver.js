31fa9240aab2791f51291a316bd01ce47f26a372
Charles Julian Knight <charles@rabidaudio.com>
2013-11-17 02:41:08 -0500

    A thing of beauty: Abstract resorces

--- express_test/eserver.js
+++ express_test/eserver.js
@@ -1,7 +1,10 @@
 var express = require('express');
-
+var config = require('./config');
+var scriptmaster = require('./scriptmaster');
 //scripts
-var wrek = require('./wrek');
+var wrek = new scriptmaster.Script(config.scripts[0]);
+//var wrek = new scriptmaster.Script();
+//console.log(wrek.script);
 
 //express settings
 var app = express();
@@ -9,13 +12,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.bodyParser());//for POST
 
 
-app.get( '/wrek/:id?', wrek.handle ); //DIR method
+app.get( '/wrek/:date?', function(req, res){
+    wrek.dir_handle(req, res);//This is some fuckedup repugnant shit
+}); //DIR method
 
-app.post('/post', function(req, res){ //POST method
-    console.log("post req");
-    console.log(req.body);
-    
-    res.send("cool yo");
-});
+app.post('/wrek', function(req, res){ wrek.post_handle(req, res); } );     //POST method
 
 app.listen(3000);
